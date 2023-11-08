b1f3a2b936020ffd26b622383e6df39a76955d7f
Forhad Reza <forhadreza55@gmail.com>
2023-11-08 18:48:55 +0600

    2nd commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Next_Byte
+
+Next_Byte A Video Sharing Flutter App. using Cloud Firestore - Firebase Video Hosting as Backend.
---
+++ lib/auth/login_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  static const String routeName = '/login';
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('login'),
+      ),
+    );
+  }
+}
