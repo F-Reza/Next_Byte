4e25ff01d9d11f75c4dedc55bfa977445d669be6
Forhad Reza <forhadreza55@gmail.com>
2023-12-19 22:29:20 +0600

    fix Cached_network_image

--- lib/views/search/user_profile_screen.dart
+++ lib/views/search/user_profile_screen.dart
@@ -1,4 +1,5 @@
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:next_byte/auth/firebase_auth.dart';
